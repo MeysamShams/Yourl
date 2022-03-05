b6570573403093bff114d7e673b0245452e6df5e
Your Name <you@example.com>
2022-03-05 20:41:51 +0330

    Initial Commit ...

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  // fastify setup
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({logger:true})
+  );
+
+  // swagger configuration
+  const config = new DocumentBuilder()
+  .setTitle('Yourl API\'s')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(3000);
+}
+bootstrap();
